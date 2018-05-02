ac27c93ae576feeb5abfb4e0563ffbc984c89796
nihil1964 <endre.beda@htl-ottakring.ac.at>
2018-05-02 12:39:55 +0200

    Helper Class added.

---
+++ helper.h
@@ -0,0 +1,12 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+#include <QObject>
+
+class Helper
+{
+public:
+    Helper();
+};
+
+#endif // HELPER_H
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 int main(int argc, char *argv[])
 {
+    // Comment
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
